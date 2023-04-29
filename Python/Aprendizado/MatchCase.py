42621d52eb521ee70ec2deeb0ba94dcdd0ba9905
LucasKeller1208 <RM551073@fiap.com.br>
2023-04-28 22:47:58 -0300

    commit

---
+++ Python/Aprendizado/MatchCase.py
@@ -0,0 +1,12 @@
+print("Bem vindo a Estação Fiap 1ESPV")
+print("Escola o destino abaixo para selecionar o número da plataforma")
+print("São Paulo")
+print("Bangu")
+print("Osasco")
+print("Argentina")
+
+Match vinho:
+
+case 1:
+print(")
+
